f1e019f20dc0bddb6387212f023bc1d7a48ced03
artemii <mr.y0lik@gmail.com>
2022-04-29 17:54:59 +0300

    v4.0 final beta

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import time
-
+import random
 import sqlite3
 
-
+nameedit = "a"
+ageedit = 5
+posedit = "c"
 
 connection = sqlite3.connect("database.sl3", 5)
 
 cur = connection.cursor()
 
-cur.execute(f"CREATE TABLE Workers(Name TEXT, Age INTEGER, Position TEXT);")
+# cur.execute(f"CREATE TABLE Workers(Name TEXT, Age INTEGER, Position TEXT);")
 
 cur.execute("INSERT INTO Workers (Name, Age, Position) VALUES "
             "('John', 32, 'Director'), "
@@ -16,12 +18,40 @@ cur.execute("INSERT INTO Workers (Name, Age, Position) VALUES "
             "('Maryana', 34, 'Secretary'), "
             "('Max', 25, 'Programmer');")
 
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+
+class Worker(Human):
+    def __init__(self, position, name, age):
+        super().__init__(name, age)
+        self.position = position
+
+    def infow(self):
+        print(f"Name - {self.name}. Age - {self.age}. Position - {self.position}.")
+
+
+class Student(Human):
+    def __init__(self, form, name, age):
+        super().__init__(name, age)
+        self.form = form
+
+    def infos(self):
+        print(f"Name - {self.name}. Age - {self.age}. Form - {self.form}.")
+
+maryana = Worker("Secretary", "Maryana", 34)
+max = Worker("Programmer", "Max", 25)
+andrei = Worker("Marketing", "Andrei", 42)
+john = Worker("Director", "John", 32)
+
+print("\t\t\t\t GOOGLE\n\t\t\t\tDATABASE\n")
 
 while True:
     try:
-        inname=input(str("Write your name to login:\nMaryana\tMax\tAndrei\tJohn\n\n"))
-        time.sleep(2)
+        inname=input(str("\t\tWrite your name to login:\n\n\tMaryana\t\tMax\t\tAndrei\t\tJohn\n\n"))
+        time.sleep(1.5)
         break
 
     except:
@@ -29,28 +59,56 @@ while True:
 
 
 
-if inname !== "Maryana" or "Max" or "Andrei" or "John":
-    print("Your name is not in the database.")
+if inname == "Maryana" or "Max" or "Andrei" or "John":
+    print("Google database:")
+    maryana.infow()
+    max.infow()
+    andrei.infow()
+    john.infow()
+    chooseact = input("\n\tI understand you! What do you want to do with database?\n"
+                      "Delete my info(Delete)\tRandom edit my info(Random)\tClose app(Close)\n")
 
 else:
-    chooseact = input("I find your name in the database! What do you want to do with it?\n"
-                      "\tDelete\tEdit\tSee\n")
+    print("ERROR 404")
 
 
 
 if chooseact == "Delete":
-    cur.execute(f"DELETE FROM Workers WHERE Name={inname};")
+    time.sleep(1)
+    print("Deleting successfully.\n\nGoogle 2022")
 
-elif chooseact == "Edit":
-    nameedit = input("Write the name that you want to write? ")
-    ageedit = input("Write the age that you want to write? ")
-    posedit = input("Write the position that you want to write? ")
-    cur.execute(f"DELETE FROM Workers WHERE Name={inname};")
-    cur.execute(f"INSERT INTO Workers (Name, Age, Position) VALUES ({nameedit}, {ageedit}, {posedit})
+elif chooseact == "Random":
+    acb = random.randint(1,3)
+    if acb == 1:
+        nameedit = "Denis"
+        ageedit = 67
+        posedit = "Director"
+
+    elif acb == 2:
+        nameedit = "Ben"
+        ageedit = 45
+        posedit = "Marketing"
+
+    elif acb == 3:
+        nameedit = "Bob"
+        ageedit = 23
+        posedit = "Secretary"
+
+
+
     print("Successfully.")
+    time.sleep(1.2)
+    print(f"\nYour new info:")
+    time.sleep(0.8)
+    print(f"\nName - {nameedit},"
+          f"\nAge - {ageedit},"
+          f"\nPosition - {posedit}.")
+    time.sleep(1.4)
+    print("\n\nGoogle 2022")
 
-
-
+elif chooseact == "Close":
+    print("OK. Closing..\nGoogle 2022")
+    time.sleep(2)
 
 
 connection.close()
